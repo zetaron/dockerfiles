cf09ccb99a71d3962e48d68bec406630d18beb4e
Fabian Stegemann <fabian.stegemann@tudock.de>
2017-01-26 11:13:42 +0000

    Rename docker images so the docker hub build process can benefit from the image inheritence

--- magento-1.9/build.sh
+++ magento-1.9/build.sh
@@ -1,3 +1,3 @@
-docker build -t zetaron/magento-1.9:php-apache php-apache
-docker build -t zetaron/magento-1.9:php-apache-xdebug php-apache-xdebug
-docker build -t zetaron/magento-1.9:tools tools
+docker build -t zetaron/magento-1.9_php-apache php-apache
+docker build -t zetaron/magento-1.9_php-apache-xdebug php-apache-xdebug
+docker build -t zetaron/magento-1.9_tools tools
